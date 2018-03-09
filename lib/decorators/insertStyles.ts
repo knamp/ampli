c6921d08b1674785bc2a65a59d5067773f3c6248
Hans Christian Reinl <info@drublic.de>
2018-03-09 17:17:56 +0100

    Filter some invalid CSS properties and rules

--- lib/decorators/insertStyles.ts
+++ lib/decorators/insertStyles.ts
@@ -1,3 +1,4 @@
+import * as Css from 'css'
 import * as CleanCSS from 'clean-css'
 
 import IDocument from '../interfaces/IDocument'
@@ -8,6 +9,10 @@ import {
   getElementContent,
 } from '../utis/'
 import strip from '../strip'
+
+const cleanCss: CleanCSS = new CleanCSS({
+  level: 2
+})
 
 const getInlineStyles = async (context: IDocument) => {
   const stylesheetContent: string[] = await getStylesheets(context)
@@ -40,11 +45,56 @@ const getStylesheets = async ({ document }: IDocument): Promise<string[]> => {
   return []
 }
 
+const filterRules = (rules: any[]): any[] => {
+  return rules.map(rule => {
+
+    // Remove @page, remove @media print
+    if (['page'].indexOf(rule.type) > -1 || ['print'].indexOf(rule.media) > -1) {
+      rule.rules = []
+
+      return rule
+    }
+
+    if (rule.rules) {
+      rule.rules = filterRules(rule.rules)
+
+      return rule
+    }
+
+    if (rule.declarations) {
+      rule.declarations = rule.declarations.map((declaration) => {
+        if (declaration.value.indexOf('!important') > -1) {
+          declaration.value = declaration.value.replace('!important', '')
+        }
+
+        return declaration
+      })
+    }
+
+    return rule
+  })
+}
+
+const filterStyles = (styles: string): string => {
+  const ast = Css.parse(styles)
+
+  ast.stylesheet.rules = filterRules(ast.stylesheet.rules)
+
+  return Css.stringify(ast)
+}
+
+const filterAndMinifyStyles = (styles: string): string => {
+  const filteredStyles = filterStyles(styles)
+  const minified = cleanCss.minify(filteredStyles)
+
+  return minified.styles
+}
+
 export default async (
   context: IDocument
 ): Promise<IDocument> => {
   const styles: string = await getInlineStyles(context)
-  const minifiedStyles: string = new CleanCSS().minify(styles).styles
+  const minifiedStyles: string = filterAndMinifyStyles(styles)
 
   context = strip(context, 'style, link')
 
