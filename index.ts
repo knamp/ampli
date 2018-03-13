4ab1edb03f12e03c59cb9737ce30d803d128a51d
Hans Christian Reinl <info@drublic.de>
2018-03-13 13:28:21 +0100

    Also export IOptions interface

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import Transformer from './lib/Transformer'
 export { default as IDocument } from './lib/interfaces/IDocument'
+export { default as IOptions } from './lib/interfaces/IOptions'
 
 export default Transformer
