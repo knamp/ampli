5312f7a7c45877e9930cf449f770823fd8306a2b
Hans Christian Reinl <info@drublic.de>
2018-03-19 10:39:40 +0100

    Fix Logger interface

--- lib/interfaces/LoggerInterface.ts
+++ lib/interfaces/LoggerInterface.ts
@@ -1,5 +1,6 @@
 export default interface LoggerInterface {
-  info: (arg1: any, arg2?: any) => void;
-  error: (arg: any, arg2?: any) => void;
-  debug: (arg: any, arg2?: any) => void;
+  info: (arg: any, ...arg2: any[]) => void;
+  error: (arg: any, ...arg2: any[]) => void;
+  warn: (arg: any, ...arg2: any[]) => void;
+  debug: (arg: any, ...arg2: any[]) => void;
 }
