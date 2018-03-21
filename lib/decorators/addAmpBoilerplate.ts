d63ad2785bd2988f4fe578b8a97bf94e9807e45c
Hans Christian Reinl <info@drublic.de>
2018-03-21 16:54:33 +0100

    Fix boilerplate amp code path, copy boilerplate

--- lib/decorators/addAmpBoilerplate.ts
+++ lib/decorators/addAmpBoilerplate.ts
@@ -7,7 +7,7 @@ export default (
   context: ContextInterface,
   options?: OptionsInterface,
 ): ContextInterface => {
-  const boilerplatePath: string = path.resolve(process.cwd(), "lib/boilerplate", "boilerplate.html");
+  const boilerplatePath: string = path.resolve(__dirname, "../boilerplate", "boilerplate.html");
   const boilerplate: Buffer = fs.readFileSync(boilerplatePath);
 
   context.document.head.innerHTML += boilerplate.toString();
