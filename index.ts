aec8b97c565f21f7436f307090e1ed4df5faad27
Hans Christian Reinl <info@drublic.de>
2018-03-22 15:41:19 +0100

    Add addAllAttributes to exports of ampli

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import Transformer from "./lib/Transformer";
 
 export { default as ContextInterface } from "./lib/interfaces/ContextInterface";
 export { default as OptionsInterface } from "./lib/interfaces/OptionsInterface";
+export { addAllAttributes } from "./lib/utils/";
 export { createElement } from "./lib/utils/";
 
 export default Transformer;
