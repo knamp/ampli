0ba29997deb33c90ef2d9d913b7e54a9c7ba2a50
Hans Christian Reinl <info@drublic.de>
2018-03-22 16:23:27 +0100

    Make it possible to use Additional scripts in options

--- lib/decorators/addAmpScript.ts
+++ lib/decorators/addAmpScript.ts
@@ -3,10 +3,22 @@ import OptionsInterface from "../interfaces/OptionsInterface";
 
 import packageConfig from "../../config";
 import { createElement } from "../utils/";
+import AdditionalScriptsInterface from "../interfaces/AdditionalScriptsInterface";
 
 const addScript = (element: HTMLElement): HTMLElement => {
   element.setAttribute("async", "");
   element.setAttribute("src", packageConfig.ampScript);
+
+  return element;
+};
+
+const addAdditonalScript = (
+  script: AdditionalScriptsInterface,
+  element: HTMLElement,
+): HTMLElement => {
+  element.setAttribute("async", "true");
+  element.setAttribute("custom-element", script.element);
+  element.setAttribute("src", script.src);
 
   return element;
 };
@@ -16,8 +28,20 @@ export default async (
   options?: OptionsInterface,
 ): Promise<ContextInterface> => {
   const element = await createElement(context, "script", addScript);
-
   context.document.head.appendChild(element);
+
+  if (options && options.additionalScripts) {
+    options.additionalScripts
+      .forEach(async (script: AdditionalScriptsInterface) => {
+        createElement(
+          context,
+          "script",
+          addAdditonalScript.bind(null, script),
+        ).then((scriptElement) => {
+          context.document.head.appendChild(scriptElement);
+        });
+      });
+  }
 
   return context;
 };
--- lib/interfaces/OptionsInterface.ts
+++ lib/interfaces/OptionsInterface.ts
@@ -1,8 +1,10 @@
+import AdditionalScriptsInterface from "./AdditionalScriptsInterface";
 import LoggerInterface from "./LoggerInterface";
 
 export default interface OptionsInterface {
   logger?: LoggerInterface;
   useStaticCss?: boolean;
   additionalTags?: string[];
+  additionalScripts?: AdditionalScriptsInterface[];
   baseUrl?: string;
 }
