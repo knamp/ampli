601839d8dba8de27d28807ceecef3cb9f2de43de
Hans Christian Reinl <info@drublic.de>
2018-04-04 09:21:02 +0200

    Add test for addtional decorators

--- __tests__/Transformer.test.ts
+++ __tests__/Transformer.test.ts
@@ -3,14 +3,31 @@ import * as path from "path";
 import Transformer from "../lib/Transformer";
 
 describe("Transformer", () => {
+  const htmlPath: string = path.resolve(__dirname, "./mock/index.html");
+  const html: string = fs.readFileSync(htmlPath).toString();
+
   it("initializes", async () => {
-    const htmlPath: string = path.resolve(__dirname, "./mock/index.html");
     const ampPath: string = path.resolve(__dirname, "./mock/index.amp.html");
-    const html: string = fs.readFileSync(htmlPath).toString();
+    const ampExpected: string = fs.readFileSync(ampPath).toString();
+
     const transformer: Transformer = new Transformer();
     const amp: string = await transformer.transform(html, "http://canoni.cal/");
 
+    expect(amp.trim()).toEqual(ampExpected.trim());
+  });
+
+  it("initializes with addtional decorators", async () => {
+    const ampPath: string = path.resolve(__dirname, "./mock/additional.amp.html");
     const ampExpected: string = fs.readFileSync(ampPath).toString();
+
+    const transformer: Transformer = new Transformer({}, [
+      (context) => {
+        context.document.documentElement.setAttribute("lang", "en");
+
+        return context;
+      },
+    ]);
+    const amp: string = await transformer.transform(html, "http://canoni.cal/");
 
     expect(amp.trim()).toEqual(ampExpected.trim());
   });
